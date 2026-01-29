430288203be07015b76f941f1aa574ab1e8f5a30
adrian <adrianvolpemath@gmail.com>
2026-01-29 14:40:50 -0600

    added the ability to hold a fruit and drop it
    new update: can hold and drop fruit

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour{
     public float speed;
+    public GameObject fruit;
+    public float offY;
 
     void Start(){
        
@@ -10,6 +12,26 @@ public class PlayerBehaviour : MonoBehaviour{
 
     
     void Update(){
+
+        if(fruit != null){
+            Vector3 playerPos = transform.position;
+            Vector3 fruitOffset = new Vector3(0.0f, offY, 0.0f);
+            fruit.transform.position = playerPos + fruitOffset;
+            Rigidbody2D body = fruit.GetComponent<Rigidbody2D>();
+            body.gravityScale = 0f;
+            Collider2D collider = fruit.GetComponent<Collider2D>();
+            collider.enabled = false;
+        }
+
+        if(Keyboard.current.spaceKey.wasPressedThisFrame){
+            Rigidbody2D body = fruit.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+            Collider2D collider = fruit.GetComponent<Collider2D>();
+            collider.enabled = true;
+            fruit = null;
+
+        }
+
         if(Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed){
             Vector3 newPosition = transform.position;
             newPosition.x = newPosition.x - speed;
